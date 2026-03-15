038583b49bae7dc0f5a5d5451bfe302b15412e66
vhadau_t <vhadau.dev@gmail.com>
2026-03-15 09:41:24 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
 import { SessionProvider } from "next-auth/react";
-import "../styles/globals.css"; // optional if you have global CSS
 
 export default function App({ Component, pageProps }) {
-  return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+  return <SessionProvider session={pageProps.session}><Component {...pageProps} /></SessionProvider>;
 }

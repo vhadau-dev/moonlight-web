070e0622907a4404a95637bb1b00212b299148a9
vhadau_t <vhadau.dev@gmail.com>
2026-03-15 08:40:23 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,10 @@
 
+import { SessionProvider } from "next-auth/react";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}

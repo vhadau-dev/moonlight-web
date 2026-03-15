88e33f83d9e5f077dfa0a2865680cc907933ed74
vhadau_t <vhadau.dev@gmail.com>
2026-03-15 08:08:59 +0200

    Create [...nextauth].js

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+
+export default NextAuth({
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      authorization: { params: { scope: "identify" } },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.id = token.sub;
+      return session;
+    },
+  },
+});

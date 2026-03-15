b0e7133d78000fa35270d2b99f5d634225e9ff95
vhadau_t <vhadau.dev@gmail.com>
2026-03-15 09:35:32 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic";
 
+// Disable SSR for homepage
 const Homepage = dynamic(() => import("../components/Homepage"), { ssr: false });
 
 export default function Home() {
